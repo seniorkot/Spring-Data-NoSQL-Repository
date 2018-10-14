be72f05148b299050890498f8ddfe51f55c24f08
seniorkot <seniorkot24@yandex.ru>
2018-10-15 01:56:35 +0300

    Refactor CassandraConfig file

--- src/main/java/ru/ifmo/se/sdbrep/config/CassandraConfig.java
+++ src/main/java/ru/ifmo/se/sdbrep/config/CassandraConfig.java
@@ -24,20 +24,37 @@
 
 package ru.ifmo.se.sdbrep.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-// TODO: Document & refactor
+/**
+ * This class is used for Spring Data Cassandra configuration.
+ *
+ * @author seniorkot
+ * @version 1.0
+ * @since 1.0
+ */
 @Configuration
+@PropertySource(value = { "classpath:cassandra.properties" })
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    private static final String KEYSPACE = "sdbrep";
+    @Value("${cassandra.contact-points}")
+    private String PROPERTY_CONTACT_POINTS;
+
+    @Value("${cassandra.port}")
+    private int PROPERTY_PORT;
+
+    @Value("${cassandra.keyspace}")
+    private String PROPERTY_KEYSPACE;
 
     @Override
     public SchemaAction getSchemaAction() {
@@ -45,29 +62,46 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     }
 
     @Override
-    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
-        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE);
-        return Collections.singletonList(specification);
+    protected String getContactPoints() {
+        return PROPERTY_CONTACT_POINTS;
     }
 
     @Override
-    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
-        return Collections.singletonList(DropKeyspaceSpecification.dropKeyspace(KEYSPACE));
-    }
-
-    @Override
-    protected List<String> getStartupScripts() {
-        return Collections.singletonList("CREATE TABLE IF NOT EXISTS " + KEYSPACE +
-                ".logs(log_id uuid PRIMARY KEY, action_text text, log_time timestamp, profile_id text, project_id text);");
+    protected int getPort() {
+        return PROPERTY_PORT;
     }
 
     @Override
     protected String getKeyspaceName() {
-        return KEYSPACE;
+        return PROPERTY_KEYSPACE;
     }
 
     @Override
     public String[] getEntityBasePackages() {
         return new String[]{"ru.ifmo.se.sdbrep.domain"};
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        CreateKeyspaceSpecification specification =
+                CreateKeyspaceSpecification.createKeyspace(getKeyspaceName()).ifNotExists();
+        return Collections.singletonList(specification);
+    }
+
+    @Override
+    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
+//        DropKeyspaceSpecification specification =
+//                DropKeyspaceSpecification.dropKeyspace(getKeyspaceName()).ifExists();
+//        return Collections.singletonList(specification);
+        return Collections.singletonList(null);
+    }
+
+    @Override
+    protected List<String> getStartupScripts() {
+        List<String> scripts = new ArrayList<>();
+        scripts.add("CREATE TABLE IF NOT EXISTS " + getKeyspaceName() +
+                ".logs(log_id uuid PRIMARY KEY, action_text text, " +
+                "log_time timestamp, profile_id text, project_id text);");
+        return scripts;
     }
 }
